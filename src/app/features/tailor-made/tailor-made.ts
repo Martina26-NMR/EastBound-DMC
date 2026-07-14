cc77a3c1552e81e6c29f9789db52b99a1e76d0de
Martina26-NMR <martinatnmr@gmail.com>
2026-07-15 00:34:22 +0300

    updated

--- src/app/features/tailor-made/tailor-made.ts
+++ src/app/features/tailor-made/tailor-made.ts
@@ -1,29 +1,29 @@
-import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, afterNextRender } from '@angular/core';
 
 @Component({
   selector: 'app-tailor-made',
   templateUrl: './tailor-made.html',
   styleUrls: ['./tailor-made.scss']
 })
-export class TailorMade implements OnInit, AfterViewInit {
+export class TailorMade implements OnInit {
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef) {
+
+    afterNextRender(() => {
+      const cards = this.el.nativeElement.querySelectorAll('.reveal-card');
+
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('active'); 
+            observer.unobserve(entry.target); 
+          }
+        });
+      }, { threshold: 0.15 }); 
+
+      cards.forEach((card: any) => observer.observe(card));
+    });
+  }
 
   ngOnInit(): void {}
-
-  ngAfterViewInit() {
-    // التقاط جميع الكروت اللي محتاجه تظهر بالأنيميشن
-    const cards = this.el.nativeElement.querySelectorAll('.reveal-card');
-
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('active'); // إضافة كلاس التفعيل
-          observer.unobserve(entry.target); // إيقاف المراقبة للكارت ده لتوفير الرام وسرعة الأداء
-        }
-      });
-    }, { threshold: 0.15 }); // يشتغل لما يظهر 15% من الكارت
-
-    cards.forEach((card: any) => observer.observe(card));
-  }
 }
